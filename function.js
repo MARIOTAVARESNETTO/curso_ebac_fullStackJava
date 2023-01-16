bbb35fe3644dcbe41be4d6d7a14b8b934eba82de
MARIOTAVARESNETTO <47722950+MARIOTAVARESNETTO@users.noreply.github.com>
2023-01-16 00:35:45 -0300

    primeiro função

---
+++ function.js
@@ -0,0 +1,10 @@
+function calculaArea(base, altura){
+   
+  return alert("Valor da área total" +' '+ base*altura);
+
+}
+
+let x = prompt("Digite o valor da base")
+let y = prompt("Digite o valor da altura")
+
+calculaArea(x,y);
